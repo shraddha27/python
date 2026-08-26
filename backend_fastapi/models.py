8fa22a1523168fbcddcfd93915145813ef8a22a0
nhsbsa-ma-shban <ma.shraddha.bandekar@nhsbsa.nhs.uk>
2026-08-26 18:24:10 +0530

    add dist

--- backend_fastapi/models.py
+++ backend_fastapi/models.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from pgvector.sqlalchemy import Vector
 
-DEFAULT_DATABASE_URL = "postgresql://postgres:root@host:5432/postgres"
+DEFAULT_DATABASE_URL = "postgresql://postgres:root@postgres:5432/postgres"
 DATABASE_URL = os.getenv("DATABASE_URL", DEFAULT_DATABASE_URL)
 
 connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
